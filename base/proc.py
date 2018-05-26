0b30e1f43219a8660eda8f5aaad0540caaab2d62
Igor Okladnikov <oig@ngs.ru>
2018-05-26 20:44:01 +0700

    Dataset, scenario, resolution and time step info is loaded from metadb

--- base/proc.py
+++ base/proc.py
@@ -2,9 +2,8 @@
 Loads a processing module and runs it providing a corresponding data access API and error handling
 """
 
-import importlib
-
 from base.dataaccess import DataAccess
+from base.common import load_module
 
 class Proc:
     def __init__(self, inputs, outputs, metadb_info):
@@ -17,23 +16,6 @@ class Proc:
         """
         self._data_helper = DataAccess(inputs, outputs, metadb_info)
 
-    def _load_module(self, module_name, class_name):
-        """Loads module by its name and returns class to instantiate
-
-        Arguments:
-            module_name -- name of the Python module (file name)
-            class_name -- name of the class in this module
-        """
-        try:
-            module_ = importlib.import_module(module_name)
-            try:
-                class_ = getattr(module_, class_name)
-            except AttributeError:
-                print("(MainApp::load_module) Class " + class_name + " does not exist")
-        except ImportError:
-            print("(MainApp::load_module) Module " + module_name + " does not exist")
-        return class_ or None
-
     def run(self, proc_class_name):
         """Creates an instance of the processing module class and runs it
         
@@ -41,7 +23,7 @@ class Proc:
             class_name -- name of the processing class
         """
         # let's try to create an instance of the processing class
-        proc_class = self._load_module("mod", proc_class_name)
+        proc_class = load_module("mod", proc_class_name)
         processor = proc_class(self._data_helper)
 
         # and here we run the module
--- mod/calc/calctimean.py
+++ mod/calc/calctimean.py
@@ -1,7 +1,9 @@
 """Sample class"""
 
+from base.dataaccess import DataAccess
+
 class cvcCalcTiMean():
-    def __init__(self, data_helper):
+    def __init__(self, data_helper: DataAccess):
         self._data_helper = data_helper
     
     def run(self):
@@ -12,6 +14,9 @@ class cvcCalcTiMean():
         time_segments = self._data_helper.get_segments(input_uids[0])
 
         vertical_levels = self._data_helper.get_levels(input_uids[0])
+
+        for segment in time_segments:
+            result = self._data_helper.get(input_uids[0], segment, vertical_levels)
 
         print("(cvcCalcTiMean::run) Finished!")
         

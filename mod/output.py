05821ed268bd4517ba7ab7b3ca14ccbf924752f3
Igor Okladnikov <oig@ngs.ru>
2018-06-25 23:00:16 +0700

    First steps

--- mod/output.py
+++ mod/output.py
@@ -6,3 +6,16 @@ class cvcOutput:
         
     def run(self):
         print("(cvcOutput::run) Started!")
+
+        input_uids = self._data_helper.input_uids()
+
+        time_segments = self._data_helper.get_segments(input_uids[0])
+
+        vertical_levels = self._data_helper.get_levels(input_uids[0])
+
+        # Get data for all time segments and levels at once
+        result = self._data_helper.get(input_uids[0], time_segments, vertical_levels)
+
+        output_uids = self._data_helper.output_uids()
+
+        pass

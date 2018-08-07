927c7cc7bf3bc5c5b2edcdf2b97c15a373819957
Igor Okladnikov <oig@ngs.ru>
2018-08-07 11:50:25 +0700

    ImageGeotiff class import added

--- mod/data/__init__.py
+++ mod/data/__init__.py
@@ -1,5 +1,5 @@
 from .datanetcdf import DataNetcdf
 from .dataparameter import DataParameter
 from .dataarray import DataArray
-from .dataimage import DataImage
+from .dataimage import DataImage, ImageGeotiff
 from .dataraw import DataRaw

97e5c2d55d5e705ec35c0c6641d467f0b9f9ad11
Igor Okladnikov <oig@ngs.ru>
2018-08-28 17:40:29 +0700

    Parameters reading from task-file was added.

--- mod/data/dataparameter.py
+++ mod/data/dataparameter.py
@@ -3,8 +3,40 @@
 """
 
 class DataParameter:
-    def __init__(self, data_info):
-        self.data_info = data_info
+    """ Provides methods for reading and writing parameters in a task file.
+    """
 
-    def read(self,options):
-        pass
+    def __init__(self, data_info):
+        self._data_info = data_info
+
+    def _type_cast(self, string_value, cast_type):
+        """ Casts string values to the specified type.
+
+        Arguments:
+            string_value -- string containig value/values to convert.
+            cast_type -- type to cast to.
+
+        Returns:
+            result -- scalar or list of scalars of the specified type.
+        """
+
+        if cast_type == 'string':
+            return string_value
+
+
+    def read(self, options):
+        """Reads parameters.
+
+        Arguments:
+            options -- dictionary of read options:
+
+        Returns:
+            result -- dictionary containing parameters from a task file
+        """
+
+        parameters = self._data_info['data']['param']
+        result = {}
+        for parameter in parameters:
+            result[parameter['@uid']] = self._type_cast(parameter['#text'], parameter['@type'])
+
+        return result

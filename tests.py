7032204f79b9c39dbe55eb3b62e34db7a1bc47c7
Igor Okladnikov <oig@ngs.ru>
2018-12-14 12:35:45 +0700

    New unit testing module for MainApp class.

---
+++ tests.py
@@ -0,0 +1,20 @@
+""" Unit testing module """
+
+import unittest
+from argparse import Namespace
+
+from base.main_app import MainApp
+
+
+class MainApplication(unittest.TestCase):
+    ''' Tests main application class'''
+
+    def test_pass_taskfile_as_argument(self):
+        '''Passing task file name as argparse arguments list should create a MainApp object'''
+        args = Namespace(task_file_name='./tasks/CalcTimean.tmpl.xml')
+
+        resut = MainApp(args)
+        self.assertTrue(isinstance(resut, MainApp))
+
+if __name__ == '__main__':
+    unittest.main()

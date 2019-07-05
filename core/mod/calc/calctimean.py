9a45680507d4b2b60560553d1c72e7a8f314838f
Igor Okladnikov <oig@ngs.ru>
2019-07-05 20:31:31 +0700

    Refactoring

--- core/mod/calc/calctimean.py
+++ core/mod/calc/calctimean.py
@@ -9,6 +9,8 @@ from core.base.common import print  # pylint: disable=W0622
 
 MAX_N_INPUT_ARGUMENTS = 2
 INPUT_PARAMETERS_INDEX = 1
+CALC_MODE = 'timeMean'
+DEFAULT_MODE = 'data'
 
 class cvcCalcTiMean():
     """ Performs calculation of time averaged values.
@@ -30,6 +32,8 @@ class cvcCalcTiMean():
         # Get parameters
         if len(input_uids) == MAX_N_INPUT_ARGUMENTS:
             parameters = self._data_helper.get(input_uids[INPUT_PARAMETERS_INDEX])
+            if parameters.get(CALC_MODE) is None:  # If the calculation mode is not set...
+                parameters[CALC_MODE] = DEFAULT_MODE  # give it a default value.
 
         # Get outputs
         output_uids = self._data_helper.output_uids()
@@ -45,17 +49,22 @@ class cvcCalcTiMean():
             for segment in time_segments:
                 # Let's calulate time averaged values
                 one_segment_mean = result['data'][level][segment['@name']]['@values'].mean(axis=0)
-                if parameters['timeMean'] == 'segment':
-                    self._data_helper.put(output_uids[0], values=one_segment_mean, level=level, segment=segment, longitudes=result['@longitude_grid'], latitudes=result['@latitude_grid'], fill_value=result['@fill_value'], meta=result['meta'])
+
+                if parameters[CALC_MODE] == 'segment':
+                    self._data_helper.put(output_uids[0], values=one_segment_mean, level=level, segment=segment,
+                                          longitudes=result['@longitude_grid'], latitudes=result['@latitude_grid'],
+                                          fill_value=result['@fill_value'], meta=result['meta'])
                 else:
                     all_segments_means.append(one_segment_mean)
 
-            if parameters['timeMean'] == 'data':
+            if parameters[CALC_MODE] == 'data':
                 data_mean = ma.stack(all_segments_means).mean(axis=0)
+
                 # Make a global segment covering all input time segments
-                full_range_segment = copy(time_segments[0])
-                full_range_segment['@ending'] = time_segments[-1]['@ending']
-                full_range_segment['@name'] = 'GlobalSeg'
+                full_range_segment = copy(time_segments[0])  # Take the beginning of the first segment...
+                full_range_segment['@ending'] = time_segments[-1]['@ending']  # and the end of the last one.
+                full_range_segment['@name'] = 'GlobalSeg'  # Give it a new name.
+
                 self._data_helper.put(output_uids[0], values=data_mean, level=level, segment=full_range_segment,
                                       longitudes=result['@longitude_grid'], latitudes=result['@latitude_grid'],
                                       fill_value=result['@fill_value'], meta=result['meta'])

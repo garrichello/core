6748aff562a4d8b4e9c4ebd5fc160d92e41394d7
Igor Okladnikov <oig@ngs.ru>
2019-07-08 15:24:59 +0700

    Separate make ROI and make mask

--- core/mod/data/data.py
+++ core/mod/data/data.py
@@ -17,14 +17,11 @@ class Data:
         self._read_result = {}   # Data arrays, grids and some additional information read in a child class.
         self._read_result['data'] = {}  # Contains data arrays read at each vertical level.
         self._data_by_segment = {}  # Data for each time segment for each vertical level.
+        
+        self._make_ROI()
 
-    def _create_ROI_mask(self, lons, lats):
-        """ Creates a 2D-mask for a given region of interest.
-        Arguments:
-            lons -- longitudes of the masked area
-            lats -- latitudes of the masked area
-        Returns:
-            mask -- 2D-mask for the area where True are masked values
+    def _make_ROI(self):
+        """ Creates region of interest for a given set of points.
         """
 
         ROI_lats_string = [p['@lat'] for p in self._data_info['data']['region']['point']]
@@ -40,21 +37,24 @@ class Data:
             print('(DataNetcdf::__init__): Bad longitude value (not a number) in data: {0}'.format(self._data_info['data']['@uid']))
             raise
 
-        ROI = [(lon, lat) for lon, lat in zip(ROI_lons, ROI_lats)]  # Region Of Interest.
+        self._ROI = [(lon, lat) for lon, lat in zip(ROI_lons, ROI_lats)]  # Region Of Interest.
 
-#        self._ROI_limits = {'min_lon' : min(ROI_lons), 'max_lon' : max(ROI_lons),
-#                            'min_lat' : min(ROI_lats), 'max_lat' : max(ROI_lats)}
+        self._ROI_bounds = {'min_lon' : min(ROI_lons), 'max_lon' : max(ROI_lons),
+                            'min_lat' : min(ROI_lats), 'max_lat' : max(ROI_lats)}
 
-#        latitude_indices = np.nonzero([ge and le for ge, le in
-#                zip(lats >= self._ROI_limits['min_lat'], lats <= self._ROI_limits['max_lat'])])[0]
-#        longitude_indices = np.nonzero([ge and le for ge, le in
-#                zip(lons >= self._ROI_limits['min_lon'], lons <= self._ROI_limits['max_lon'])])[0]
-
+    def _make_ROI_mask(self, lons, lats):
+        """ Creates a 2D-mask for a given region of interest.
+        Arguments:
+            lons -- longitudes of the masked area
+            lats -- latitudes of the masked area
+        Returns:
+            mask -- 2D-mask for the area where True are masked values
+        """
         lon2d, lat2d = np.meshgrid(lons, lats)
         lon_coords, lat_coords = lon2d.flatten(), lat2d.flatten()
         points = np.vstack((lon_coords, lat_coords)).T
 
-        path = Path(ROI)
+        path = Path(self._ROI)
         mask = path.contains_points(points) # True is for the points inside the ROI
         mask = ~mask.reshape((lats.size, lons.size)) # True is masked so we need to inverse the mask
 

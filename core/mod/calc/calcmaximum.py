20311e0b3ba1fd940d486984060b512f0fc72cc2
Igor Okladnikov <oig@ngs.ru>
2019-07-09 12:09:10 +0700

    Add new class

---
+++ core/mod/calc/calcmaximum.py
@@ -0,0 +1,26 @@
+""" Class cvcCalcMaximum provides methods for calculation of maximum over time values
+"""
+
+from core.base.dataaccess import DataAccess
+from core.base.common import print  # pylint: disable=W0622
+from core.mod.calc.calcmeanmaxmin import CalcMeanMaxMin
+
+CALC_MODE = 'timeMax'
+
+class cvcCalcMaximum(CalcMeanMaxMin):
+    """ Performs calculation of time averaged values.
+
+    """
+
+    def __init__(self, data_helper: DataAccess):
+        self._data_helper = data_helper
+        super().__init__(data_helper)
+
+    def run(self):
+        """ Main method of the class. Reads data arrays, process them and returns results. """
+
+        print('(cvcCalcMaximum::run) Started!')
+
+        self._run_common(CALC_MODE)
+
+        print('(cvcCalcMaximum::run) Finished!')

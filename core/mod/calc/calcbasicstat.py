70bf317287a263cf287c4d6b54c69faafa606545
Igor Okladnikov <oig@ngs.ru>
2019-07-13 11:21:49 +0700

    Refactoring

--- core/mod/calc/calcbasicstat.py
+++ core/mod/calc/calcbasicstat.py
@@ -63,42 +63,38 @@ class CalcBasicStat(Calc):
         for level in vertical_levels:
             all_segments_data = []
             for segment in time_segments:
+                one_segment_time_grid = []
+                
                 # Get data
                 result = self._data_helper.get(input_uids[0], segments=segment, levels=level)
 
                 # Daily statistics.
                 if parameters[calc_mode] == 'day':
                     one_segment_data = []
-                    one_segment_time_grid = []
                     data_time_iter = itertools.zip_longest(result['data'][level][segment['@name']]['@values'],
                                                            result['data'][level][segment['@name']]['@time_grid'])
-                    for key, group in itertools.groupby(data_time_iter, key=self._data_time_key):
+                    for date_key, group in itertools.groupby(data_time_iter, key=self._data_time_key):
                         group_data = []
                         for data, _ in group:
                             group_data.append(data)
                         group_data = ma.stack(group_data)
-                        one_segment_time_grid.append(key)
+                        one_segment_time_grid.append(date_key)
 
                         # Calulate time statistics for a current time group (day)
                         one_segment_data.append(stat_func(group_data, axis=0))
 
-                    # For day-wise statistics send to the output current time segment results with a new time grid
-                    data_out = ma.stack(one_segment_data)
-                    self._data_helper.put(output_uids[0], values=data_out, level=level, segment=segment,
-                                          longitudes=result['@longitude_grid'], latitudes=result['@latitude_grid'],
-                                          times=one_segment_time_grid, fill_value=result['@fill_value'], meta=result['meta'])
-
                 # Calulate time statistics for a current time segment
                 if (parameters[calc_mode] == 'data') or (parameters[calc_mode] == 'segment'):
                     one_segment_data = stat_func(result['data'][level][segment['@name']]['@values'], axis=0)
+                    one_segment_time_grid.append(result['data'][level][segment['@name']]['@time_grid'][0])
 
                 # For segment-wise averaging send to the output current time segment results
                 # or store them otherwise.
-                if parameters[calc_mode] == 'segment': 
+                if (parameters[calc_mode] == 'day') or (parameters[calc_mode] == 'segment'): 
                     self._data_helper.put(output_uids[0], values=one_segment_data, level=level, segment=segment,
                                           longitudes=result['@longitude_grid'], latitudes=result['@latitude_grid'],
-                                          fill_value=result['@fill_value'], meta=result['meta'])
-                else:
+                                          times=one_segment_time_grid, fill_value=result['@fill_value'], meta=result['meta'])
+                elif parameters[calc_mode] == 'data':
                     all_segments_data.append(one_segment_data)
 
             # For data-wise analysis analyse segments analyses :)

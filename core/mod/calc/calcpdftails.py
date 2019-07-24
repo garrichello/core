fa83a9b45c7037d26a746e32c417b93c2bd8bd4f
Igor Okladnikov <oig@ngs.ru>
2019-07-24 22:48:28 +0700

    Take only one level

--- core/mod/calc/calcpdftails.py
+++ core/mod/calc/calcpdftails.py
@@ -31,44 +31,43 @@ class CalcPDFtails(Calc):
 
         # Get time segments and levels
         time_segment = self._data_helper.get_segments(uid)[0]  # Only the first time segment is taken.
-        vertical_levels = self._data_helper.get_levels(uid)
+        level = self._data_helper.get_levels(uid)[0]  # Only the first vertical level is taken.
 
         all_percentiles = {}
         all_percentiles['data'] = {}
-        for level in vertical_levels:
-            start_date = datetime.datetime.strptime(time_segment['@beginning'], '%Y%m%d%H')
-            end_date = datetime.datetime.strptime(time_segment['@ending'], '%Y%m%d%H')
-            years = [start_date.year + i for i in range(end_date.year - start_date.year + 1)]
-            segment_start = datetime.datetime(1, start_date.month, start_date.day, start_date.hour)
-            segment_end = datetime.datetime(1, end_date.month, end_date.day, end_date.hour)
-            dates_delta = segment_end - segment_start + datetime.timedelta(days=1)  # Days in the segment.
-            days = [segment_start + datetime.timedelta(days=i) for i in range(dates_delta.days)]  # Days of the segment.
+        start_date = datetime.datetime.strptime(time_segment['@beginning'], '%Y%m%d%H')
+        end_date = datetime.datetime.strptime(time_segment['@ending'], '%Y%m%d%H')
+        years = [start_date.year + i for i in range(end_date.year - start_date.year + 1)]
+        segment_start = datetime.datetime(1, start_date.month, start_date.day, start_date.hour)
+        segment_end = datetime.datetime(1, end_date.month, end_date.day, end_date.hour)
+        dates_delta = segment_end - segment_start + datetime.timedelta(days=1)  # Days in the segment.
+        days = [segment_start + datetime.timedelta(days=i) for i in range(dates_delta.days)]  # Days of the segment.
 
-            # For each day of the year (segment) take 5-day window centered in this day for all years (30).
-            # Concatenate 5 days x 30 years to obtain 150 lon-lat grids.
-            # Calculate percentile along time axis for each cell of the grid to get 2-D grid of percentiles.
-            # Make a Masked array on the base of the percentile grid using mask from one of the 5-day arrays.
-            # We suppose that the mask is the same for all lon-lat grids along the time axis.
-            percentile = []
-            for day in days:
-                segments = []
-                for year in years:
-                    day_i = datetime.datetime(year, day.month, day.day, day.hour)
-                    five_days = {}  # Five-day segment to read.
-                    five_days['@beginning'] = (day_i - datetime.timedelta(days=2)).strftime('%Y%m%d%H')
-                    five_days['@ending'] = (day_i + datetime.timedelta(days=2, hours=23)).strftime('%Y%m%d%H')
-                    five_days['@name'] = 'Year {}'.format(year)
-                    segments.append(five_days)
-                result = self._data_helper.get(uid, segments=segments, levels=vertical_levels)
-                data = np.ma.concatenate(
-                    [result['data'][level]['Year {}'.format(year)]['@values'] for year in years], axis=0)
-                percentile.append(np.percentile(data, threshold, axis=0))
-            percentile = np.stack(percentile, axis=0)  # Stack to array.
-            mask_0 = result['data'][level]['Year {}'.format(years[0])]['@values'].mask[0]  # lon-lat mask.
-            mask_shape = [1] * (mask_0.ndim + 1)  # New shape of the mask.
-            mask_shape[0] = len(days)  # Set it to be (n_days, 1, 1) or (n_days, 1).
-            mask = np.tile(mask_0, mask_shape)  # Generate a mask to conform data dimensions.
-            all_percentiles['data'][level] = np.ma.MaskedArray(percentile, mask=mask, fill_value=result['@fill_value'])
+        # For each day of the year (segment) take 5-day window centered in this day for all years (30).
+        # Concatenate 5 days x 30 years to obtain 150 lon-lat grids.
+        # Calculate percentile along time axis for each cell of the grid to get 2-D grid of percentiles.
+        # Make a Masked array on the base of the percentile grid using mask from one of the 5-day arrays.
+        # We suppose that the mask is the same for all lon-lat grids along the time axis.
+        percentile = []
+        for day in days:
+            segments = []
+            for year in years:
+                day_i = datetime.datetime(year, day.month, day.day, day.hour)
+                five_days = {}  # Five-day segment to read.
+                five_days['@beginning'] = (day_i - datetime.timedelta(days=2)).strftime('%Y%m%d%H')
+                five_days['@ending'] = (day_i + datetime.timedelta(days=2, hours=23)).strftime('%Y%m%d%H')
+                five_days['@name'] = 'Year {}'.format(year)
+                segments.append(five_days)
+            result = self._data_helper.get(uid, segments=segments, levels=level)
+            data = np.ma.concatenate(
+                [result['data'][level]['Year {}'.format(year)]['@values'] for year in years], axis=0)
+            percentile.append(np.percentile(data, threshold, axis=0))
+        percentile = np.stack(percentile, axis=0)  # Stack to array.
+        mask_0 = result['data'][level]['Year {}'.format(years[0])]['@values'].mask[0]  # lon-lat mask.
+        mask_shape = [1] * (mask_0.ndim + 1)  # New shape of the mask.
+        mask_shape[0] = len(days)  # Set it to be (n_days, 1, 1) or (n_days, 1).
+        mask = np.tile(mask_0, mask_shape)  # Generate a mask to conform data dimensions.
+        all_percentiles['data'][level] = np.ma.MaskedArray(percentile, mask=mask, fill_value=result['@fill_value'])
 
         all_percentiles['@base_period'] = time_segment
         all_percentiles['@day_grid'] = days
@@ -76,6 +75,11 @@ class CalcPDFtails(Calc):
         all_percentiles['@latitude_grid'] = result['@latitude_grid']
         all_percentiles['@fill_value'] = result['@fill_value']
         all_percentiles['meta'] = result['meta']
+        if 'max' in result['data']['description']['@name'].lower():
+            varname = 'maxday'
+        elif 'min' in result['data']['description']['@name'].lower():
+            varname = 'minday'
+        all_percentiles['meta']['varname'] = '{}{}p'.format(varname, threshold)
 
         return all_percentiles
 
@@ -93,12 +97,10 @@ class CalcPDFtails(Calc):
         assert output_uids, '(CalcPDFtails::run) No output arguments!'
 
         # Calculate percentiles
-        input_varname = ''
         out_uid = 0
         for in_uid in input_uids:
             for threshold in THRESHOLDS:
                 percentile = self._calc_percentile(in_uid, threshold)
-                percentile['meta']['varname'] = '{}{}p'.format(input_varname, threshold)
                 for level, data in percentile['data']:
                     self._data_helper.put(output_uids[out_uid], values=data, level=level,
                                           segment=percentile['@base_period'],

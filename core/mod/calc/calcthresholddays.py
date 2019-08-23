a4074d2a6ee5b23f4a37851b28050e0971e132fa
Igor Okladnikov <oig@ngs.ru>
2019-08-23 15:12:19 +0700

    New module

---
+++ core/mod/calc/calcthresholddays.py
@@ -0,0 +1,172 @@
+""" CalcThresholdDays implements calculation of a spatial field of number of days exceeding/failing given threshold.
+
+    Input arguments:
+        input_uids[0] -- study data
+            minimum daily values -- for Frost Days and Tropical Nights
+            maximum daily values -- for Icing Days and Summer Days
+        input_uids[1] -- module parameters:
+            Condition -- string, allowed values:
+                'less' -- for Frost Days and Icing Days
+                'greater' -- for Supper Days and Tropical Nights
+            Threshold -- float, value that must not be exceeded or reached, same unita as in study data
+            Mode -- string, allowed values:
+                'segment' -- for each segment
+                'data' -- maximum over all segments
+    Output arguments:
+        output_uids[0] -- number of Frost/Icing/Summer Days or Tropical Nights, data array of size:
+            [segments, lats, lons] -- for Mode == 'segment'
+            [lats, lons] -- for Mode == 'data'
+"""
+
+from copy import deepcopy
+import operator
+import datetime
+
+import numpy as np
+import numpy.ma as ma
+
+from core.base.dataaccess import DataAccess
+from core.base.common import print  # pylint: disable=W0622
+from core.mod.calc.calc import Calc
+
+MAX_N_INPUT_ARGUMENTS = 2
+INPUT_PARAMETERS_INDEX = 1
+STUDY_UID = 0
+DEFAULT_PARAMETERS = {'Condition': 'less', 'Threshold': 273.15, 'Mode': 'data'}
+
+class CalcThresholdDays(Calc):
+    """ Provides calculation of a spatial field of cold/warm nights/days values for time series of data.
+
+    """
+
+    def __init__(self, data_helper: DataAccess):
+        self._data_helper = data_helper
+
+    def _get_parameter(self, parameter_name, parameters):
+        """ Extracts parameter value by name from a dictionary or returns a default value.
+        Arguments:
+            parameter_name -- name of the parameter
+            parameters -- dictionary of parameters
+
+        Returns: value from a parameters or default value
+        """
+        value = parameters.get(parameter_name)
+        if value is None:
+            value = DEFAULT_PARAMETERS[parameter_name]
+
+        return value
+
+    def _remove_feb29(self, values, time_grid):
+        """ Removes February 29 from data and time grid
+        Arguments:
+            data -- data values
+            time_grid -- time grid
+        """
+        try:
+            feb29 = datetime.datetime(time_grid[0].year, 2, 29, time_grid[0].hour, time_grid[0].minute)
+        except ValueError:
+            feb29 = None
+        if feb29:
+            time_list = time_grid.tolist()
+            feb29_index = time_list.index(feb29)
+            values = np.delete(values, feb29_index, axis=0)
+
+        return values
+
+    def _calc_duration(self, mask):
+        """ Calculates a duration of the longest consecutive True values.
+        Arguments:
+            mask -- mask values for each time step.
+
+        Returns:
+            m x n array of the longest consecutive True values.
+        """
+        counter = ma.zeros(mask.shape[1:])  # Current counter
+        duration = ma.zeros(mask.shape[1:])  # Longest sequence
+        for array in mask:
+            counter += array  # Increment counters in according cells.
+            counter *= array  # Reset previously accumulated counts in 0-valued cells.
+            idxs = counter > duration
+            duration[idxs] = counter[idxs]
+
+        return duration
+
+    def run(self):
+        """ Main method of the class. Reads data arrays, process them and returns results. """
+
+        print('(CalcThresholdDays::run) Started!')
+
+        # Get inputs
+        input_uids = self._data_helper.input_uids()
+        assert input_uids, '(CalcThresholdDays::run) No input arguments!'
+
+        # Get parameters
+        if len(input_uids) == MAX_N_INPUT_ARGUMENTS:  # If parameters are given.
+            parameters = self._data_helper.get(input_uids[INPUT_PARAMETERS_INDEX])
+            condition = self._get_parameter('Condition', parameters)
+            threshold = self._get_parameter('Threshold', parameters)
+            calc_mode = self._get_parameter('Mode', parameters)
+
+        print('(CalcThresholdDays::run) Calculation condition: {}'.format(condition))
+        print('(CalcThresholdDays::run) Threshold: {}'.format(threshold))
+        print('(CalcThresholdDays::run) Calculation mode: {}'.format(calc_mode))
+
+        # Get outputs
+        output_uids = self._data_helper.output_uids()
+        assert output_uids, '(CalcThresholdDays::run) No output arguments!'
+
+        # Get time segments and levels
+        study_time_segments = self._data_helper.get_segments(input_uids[STUDY_UID])
+        study_vertical_levels = self._data_helper.get_levels(input_uids[STUDY_UID])
+
+        if condition == 'less':
+            comparison_func = operator.lt
+        elif condition == 'greater':
+            comparison_func = operator.gt
+        else:
+            print('(CalcThresholdDays::run) Error! Unknown condition value: \'{}\''.format(condition))
+            raise ValueError
+
+        data_func = ma.max  # For calc_mode == 'data' we calculate max over all segments.
+
+        for level in study_vertical_levels:
+            all_segments_data = []
+            for segment in study_time_segments:
+                # Read data
+                study_data = self._data_helper.get(input_uids[STUDY_UID], segments=segment)
+                study_values = study_data['data'][level][segment['@name']]['@values']
+
+                # Compare values according chosen exceedance.
+                comparison_mask = comparison_func(study_values, threshold)
+
+                # Perform calculation for the current time segment.
+                one_segment_data = ma.sum(comparison_mask, axis=0)
+
+                # For segment-wise averaging send to the output current time segment results
+                # or store them otherwise.
+                if calc_mode == 'segment':
+                    self._data_helper.put(output_uids[0], values=one_segment_data, level=level, segment=segment,
+                                          longitudes=study_data['@longitude_grid'],
+                                          latitudes=study_data['@latitude_grid'],
+                                          fill_value=study_data['@fill_value'],
+                                          meta=study_data['meta'])
+                elif calc_mode == 'data':
+                    all_segments_data.append(one_segment_data)
+                else:
+                    print('(CalcThresholdDays::run) Error! Unknown calculation mode: \'{}\''.format(calc_mode))
+                    raise ValueError
+
+            # For data-wise analysis analyse segments analyses :)
+            if calc_mode == 'data':
+                data_out = data_func(ma.stack(all_segments_data), axis=0)
+
+                # Make a global segment covering all input time segments
+                full_range_segment = deepcopy(study_time_segments[0])  # Take the beginning of the first segment...
+                full_range_segment['@ending'] = study_time_segments[-1]['@ending']  # and the end of the last one.
+                full_range_segment['@name'] = 'GlobalSeg'  # Give it a new name.
+
+                self._data_helper.put(output_uids[0], values=data_out, level=level, segment=full_range_segment,
+                                      longitudes=study_data['@longitude_grid'], latitudes=study_data['@latitude_grid'],
+                                      fill_value=study_data['@fill_value'], meta=study_data['meta'])
+
+        print('(CalcThresholdDays::run) Finished!')

07b1db60cb68553c3cdf96b915e55b26b6c0747a
Igor Okladnikov <oig@ngs.ru>
2019-10-07 09:04:32 +0700

    Add new module

---
+++ core/mod/calc/calcsdii.py
@@ -0,0 +1,130 @@
+"""Class CalcSDII provides methods for calculation of simple precipitation intensity index (SDII).
+
+    Input arguments:
+        input_uids[0] -- total precipitation values
+        input_uids[1] -- module parameters:
+            Mode -- string, allowed values:
+                'segment' -- for each segment
+                'data' -- maximum over all segments
+
+    Output arguments:
+        output_uids[0] -- simple precipitation intensity index array:
+            [segments, lats, lons] -- for Mode == 'segment'
+            [lats, lons] -- for Mode == 'data'
+
+"""
+
+from collections import deque
+from itertools import groupby
+from copy import deepcopy
+import numpy.ma as ma
+
+from core.base.dataaccess import DataAccess
+from core.base.common import print  # pylint: disable=W0622
+from core.mod.calc.calc import Calc
+
+MAX_N_INPUT_ARGUMENTS = 2
+INPUT_PARAMETERS_INDEX = 1
+DATA_UID = 0
+DEFAULT_VALUES = {'Mode': 'data'}
+
+class CalcSDII(Calc):
+    """ Performs calculation of simple precipitation intensity index.
+
+    """
+
+    def __init__(self, data_helper: DataAccess):
+        self._data_helper = data_helper
+
+    def calc_sdii(self, values, time_grid):
+        """ Calculates SDII
+        Arguments:
+            values -- array of total precipitation
+            time_grid -- time grid
+        Returns: Rxnday values
+        """
+
+        threshold = 1  # Wet day threshold (1 mm)
+        w_days = 0  # Wet days (daily precipitation >= 1 mm) counter.
+        rr_sum = 0  # Sum of daily precipitation in wet days.
+        it_all_data = groupby(zip(values, time_grid), key=lambda x: (x[1].day, x[1].month))
+        for _, one_day_group in it_all_data:  # Iterate over daily groups.
+            daily_sum = None
+            for one_step_data, _ in one_day_group:  # Iterate over each time step in each group.
+                if daily_sum is None:
+                    daily_sum = deepcopy(one_step_data)
+                else:
+                    daily_sum += one_step_data  # Calculate daily sums.
+            if daily_sum > threshold:
+                w_days += 1
+                rr_sum += daily_sum
+        result = rr_sum / w_days
+
+        return result
+
+    def run(self):
+        """ Main method of the class. Reads data arrays, process them and returns results. """
+
+        print('(CalcSDII::run) Started!')
+
+        # Get inputs
+        input_uids = self._data_helper.input_uids()
+        assert input_uids, '(CalcSDII::run) No input arguments!'
+
+        # Get parameters
+        if len(input_uids) == MAX_N_INPUT_ARGUMENTS:  # If parameters are given.
+            parameters = self._data_helper.get(input_uids[INPUT_PARAMETERS_INDEX])
+            calc_mode = self._get_parameter('Mode', parameters, DEFAULT_VALUES)
+
+        print('(CalcSDII::run) Calculation mode: {}'.format(calc_mode))
+
+        # Get outputs
+        output_uids = self._data_helper.output_uids()
+        assert output_uids, '(CalcSDII::run) No output arguments!'
+
+        # Get time segments and levels
+        time_segments = self._data_helper.get_segments(input_uids[DATA_UID])
+        vertical_levels = self._data_helper.get_levels(input_uids[DATA_UID])
+
+        data_func = ma.max  # For calc_mode == 'data' we calculate max over all segments.
+
+        # Main loop
+        for level in vertical_levels:
+            all_segments_data = []
+            for segment in time_segments:
+                # Read data
+                data = self._data_helper.get(input_uids[DATA_UID], segments=segment, levels=level)
+                values = data['data'][level][segment['@name']]['@values']
+                time_grid = data['data'][level][segment['@name']]['@time_grid']
+
+                one_segment_data = self.calc_sdii(values, time_grid)
+
+                # For segment-wise averaging send to the output current time segment results
+                # or store them otherwise.
+                if calc_mode == 'segment':
+                    self._data_helper.put(output_uids[0], values=one_segment_data, level=level, segment=segment,
+                                          longitudes=data['@longitude_grid'],
+                                          latitudes=data['@latitude_grid'],
+                                          fill_value=data['@fill_value'],
+                                          meta=data['meta'])
+                elif calc_mode == 'data':
+                    all_segments_data.append(one_segment_data)
+                else:
+                    print('(CalcCDDCWD::run) Error! Unknown calculation mode: \'{}\''.format(calc_mode))
+                    raise ValueError
+
+            # For data-wise analysis analyse segments analyses :)
+            if calc_mode == 'data':
+                data_out = data_func(ma.stack(all_segments_data), axis=0)
+
+                # Make a global segment covering all input time segments
+                full_range_segment = deepcopy(time_segments[0])  # Take the beginning of the first segment...
+                full_range_segment['@ending'] = time_segments[-1]['@ending']  # and the end of the last one.
+                full_range_segment['@name'] = 'GlobalSeg'  # Give it a new name.
+
+                self._data_helper.put(output_uids[0], values=data_out, level=level, segment=full_range_segment,
+                                      longitudes=data['@longitude_grid'], latitudes=data['@latitude_grid'],
+                                      fill_value=data['@fill_value'], meta=data['meta'])
+
+
+        print('(CalcSDII::run) Finished!')

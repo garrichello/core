266cbb78dbbc7f7336aae86113ee95e7cf35e8e7
Igor Okladnikov <oig@ngs.ru>
2019-10-07 10:34:46 +0700

    Add calc daily sum for iter group

--- core/mod/calc/calc.py
+++ core/mod/calc/calc.py
@@ -1,7 +1,7 @@
 """ Base class cvcCalc provides methods for all calculation classes
 """
 
-from copy import copy
+from copy import deepcopy
 
 class Calc():
     """ Base class for all calculation methods
@@ -15,7 +15,7 @@ class Calc():
 
         """
 
-        full_range_segment = copy(time_segments[0])  # Take the beginning of the first segment...
+        full_range_segment = deepcopy(time_segments[0])  # Take the beginning of the first segment...
         full_range_segment['@ending'] = time_segments[-1]['@ending']  # and the end of the last one.
         full_range_segment['@name'] = 'GlobalSeg'  # Give it a new name.
 
@@ -35,3 +35,13 @@ class Calc():
             value = default_values[parameter_name]
 
         return value
+
+    def _calc_daily_sum(self, one_day_group):
+        daily_sum = None
+        for one_step_data, _ in one_day_group:  # Iterate over each time step in each group.
+            if daily_sum is None:
+                daily_sum = deepcopy(one_step_data)
+            else:
+                daily_sum += one_step_data  # Calculate daily sums.
+
+        return daily_sum

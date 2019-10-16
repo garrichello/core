81a1cd0f8e97f5778c69296a8850e46ff4ffab6f
Igor Okladnikov <oig@ngs.ru>
2019-10-16 11:52:40 +0700

    Add time grid harmonization method

--- core/mod/calc/calcunifygrids.py
+++ core/mod/calc/calcunifygrids.py
@@ -11,8 +11,6 @@ Spatial borders should be the same for both datasets. Level lists should be the 
          at the same spatial and time grids.
 """
 
-from copy import deepcopy
-
 import numpy.ma as ma
 
 from core.base.dataaccess import DataAccess
@@ -36,17 +34,74 @@ class CalcUnifyGrids(Calc):
         Normally data are averaged but some (total precipitation) are summed.
         Arguments:
             values -- data values (e.g., [time, lat, lon])
-            original_time_grid -- original time grid
-            target_time_grid -- target time grid
+            original_time_grid -- original time grid (must be finer than target)
+            target_time_grid -- target time grid (must be coarser than original)
             mode -- action mode:
                 'mean' -- data are averaged
                 'sum' -- data are summed
         Returns:
             result -- transformed along time axis data values
         """
-        
+        if len(original_time_grid) < len(target_time_grid):
+            print('(CalcUnifyGrids::_unify_time_grid) Error! \
+                Original_time_grid must be finer (contain more points) than target_time_grid!')
+            raise ValueError
 
+        if mode == 'mean':
+            acc_func = ma.mean  # Average original grid values between target grid points
+        elif mode == 'sum':
+            acc_func = ma.sum  # Sum original grid values between target grid points
+        else:
+            print('(CalcUnifyGrids::_unify_time_grid) Error! Unknown time grid harmonization mode: {}. Aborting!'.format(mode))
+            raise ValueError
 
+        # Create the result array.
+        dims = list(values.shape)
+        dims[0] = len(target_time_grid)
+        result = ma.zeros(dims)
+
+        # Reduce values over the time dimension.
+        store = []  # Stores values inside one target time grid step.
+        j = 0  # Runs along target_time_grid.
+        # If target grid has daily steps or longer we deal with averaged/total values.
+        # So we need to average or sum original grid values for each target grid step:
+        #  result(15.05.2001) = acc_func(values(15.05.2001, 00:00), values(15.05.2001, 06:00),
+        #                                values(15.05.2001, 12:00), values(15.05.2001, 18:00)).
+        if (target_time_grid[1]-target_time_grid[0]).days >= 1:
+            for i, cur_values in enumerate(values):  # i runs along original_time_grid.
+                if original_time_grid[i] >= target_time_grid[j] and \
+                    original_time_grid[i] < target_time_grid[j+1] if j < len(target_time_grid) else True:  # Control right bound.
+                    store.append(cur_values)  # Collect values inside one day/month/year
+                else:
+                    result[j] = acc_func(ma.stack(store), axis=0)  # Apply an appropriate aggregating function.
+                    store = []
+                    j += 1
+        # If target grid has n-hourly steps we deal with another time grid for instantenous or accumulated values.
+        # So we need to interpolate original grid instanteneous values to the target grid:
+        #  result(15.05.2001, 06:00) = interpolate(values(15.05.2001, 03:00), values(15.05.2001, 09:00))
+        #   or take values only at required steps:
+        #  values(00:00), [drop values(03:00)], values(06:00), [drop values(09:00)], values(12:00),...
+        # OR we need to accumulate values from previous steps:
+        #  result(15.05.2001, 06:00) = values(15.05.2001, 03:00) + values(15.05.2001, 06:00),
+        #  result(15.05.2001, 12:00) = values(15.05.2001, 09:00) + values(15.05.2001, 12:00)...
+        else:
+            if mode == 'sum':  # Accumulate accumulated values :)
+                for i, cur_values in enumerate(values):  # i runs along original_time_grid.
+                    if original_time_grid[i] <= target_time_grid[j] and \
+                        original_time_grid[i] > target_time_grid[j-1] if j > 0 else True:  # Control left bound.
+                        store.append(cur_values)  # Collect values inside one day/month/year
+                    else:
+                        result[j] = ma.sum(ma.stack(store), axis=0)  # Sum accumulated values.
+                        store = []
+                        j += 1
+            else:
+                # ToDo: May be someday there will be interpolation. But for now: only search for exact match.
+                for i, cur_values in enumerate(values):  # i runs along original_time_grid.
+                    if original_time_grid[i] == target_time_grid[j]:
+                        result[j] = cur_values
+                        j += 1
+                    if j == len(target_time_grid):
+                        break
         return 0
 
     def _unify_grids(self, data_1, data_1_add, data_2, data_2_add):

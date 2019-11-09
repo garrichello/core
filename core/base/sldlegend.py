9dfca6414689bd9ae1d90bdabf671700edcc3575
Igor Okladnikov <oig@ngs.ru>
2019-11-09 14:11:46 +0700

    Multiband write support

--- core/base/sldlegend.py
+++ core/base/sldlegend.py
@@ -2,12 +2,13 @@
     SLDLegend
 """
 
-import os.path
+import logging
 
 import xmltodict
 from matplotlib import cm, colors
 import numpy as np
 
+from core.base.common import make_raw_filename, make_filename, listify
 
 class SLDLegend:
     """Class SLDLegend.
@@ -20,15 +21,37 @@ class SLDLegend:
         Arguments:
             data_info -- global data description dictionary
         """
+        self.logger = logging.getLogger()
         self._data_info = data_info
         self._legend_options = self._data_info['data']['graphics']['legend']
 
-    def write(self, values, options):
-        """Writes a legend in an SLD file.
+    def _get_meta_value(self, options, value_name):
+        """ Returns value in options meta dictionary.
 
         Arguments:
-            values -- processing result's values as a masked array/array/list.
             options -- dictionary of write options
+            value_name -- name of meta value to extract
+
+        Returns:
+            value -- value or None if it (or meta) deoe not exist.
+        """
+
+        value = None
+        if options.get('meta') is not None:
+            if options['meta'].get(value_name) is not None:
+                value = options['meta'][value_name]
+
+        return value
+
+    def _make_legend_values(self, values, options):
+        """Prepares legend values.
+
+        Arguments:
+            values -- processing result's values as a masked array/array/list
+            options -- dictionary of write options
+
+        Returns:
+            legend_values, legend_labels, rgb_values -- legend values, tabels and RGB-mapping
         """
 
         # Get RGB values of the selected color map.
@@ -90,30 +113,63 @@ class SLDLegend:
             legend_labels = ['NO DATA!' for value in legend_values]  # Labels for each colorbar tick
             rgb_values = [colors.to_hex((0, 0, 0)) for i in idxs]  # Colors for each colorbar tick in HEX format
 
-        # Legend file name composition.
-        (file_root, file_ext) = os.path.splitext(self._legend_options['file']['@name'])
-        legend_filename = \
-            '{}_{}_{}-{}{}'.format(file_root, options['level'], options['segment']['@beginning'],
-                                   options['segment']['@ending'], file_ext)
+        return legend_values, legend_labels, rgb_values
+
+    def write(self, all_values, all_options):
+        """Writes a legend in an SLD file. Supports multiple bands (variables).
+
+        Arguments:
+            all_values -- processing result's values as a masked array/array/list (list of arrays in multiband case)
+            all_options -- dictionary of write options (list of dictionaries in multiband case)
+        """
+
+        self.logger.info('Writing legend...')
+
+        # Prepare legend values.
+        legend_properties = []
+        layer_num = 0
+        for values, options in zip(listify(all_values), listify(all_options)):
+            layer_num += 1
+            cur_leg_val, cur_leg_lab, cur_rgb_val = self._make_legend_values(values, options)
+            legend_data = {}
+            legend_data['values'] = cur_leg_val
+            legend_data['labels'] = cur_leg_lab
+            legend_data['rgb'] = cur_rgb_val
+            varname = self._get_meta_value(options, 'varname')
+            legend_data['layer_name'] = varname if varname is not None else 'Layer #{}'.format(layer_num)
+            fill_value = values.fill_value
+            if fill_value.dtype == np.dtype('bool'):  # Convert boolean fill value to numeric.
+                fill_value = int(fill_value)
+            legend_data['fill_value'] = fill_value
+            legend_properties.append(legend_data)
+
+        # Use legend options to make legend filename(s).
+        legend_info = {'data': self._legend_options}
 
         # Select writer corresponding to data kind.
         if self._legend_options['@data_kind'] == 'raster':
-            self.write_raster(legend_filename, legend_values, legend_labels, rgb_values, values.fill_value)
+            legend_filename = make_raw_filename(legend_info, all_options)
+            self.write_raster(legend_filename, legend_properties)
         if self._legend_options['@data_kind'] == 'station':
-            self.write_stations(legend_filename, legend_values, legend_labels, rgb_values)
+            for legend_data in legend_properties:
+                legend_filename = make_filename(legend_info, all_options)
+                self.write_stations(legend_filename, legend_data)
 
-    def write_raster(self, filename, legend_values, legend_labels, rgb_values, fill_value):
-        """Writes a legend for raster data into SLD file.
+        self.logger.info('Done!')
+
+    def write_raster(self, filename: str, legend_properties: list):
+        """Writes a legend for raster data into SLD file. Supports multiple bands (variables).
 
         Arguments:
             filename -- name of SLD file.
-            legend_values -- data values to be written into SLD file.
-            legend_labels -- labels corresponding to data values.
-            rgb_values -- hex-values of colors corresponding to data values.
-            fill_value -- value for transparent pixels
+            legend_properties -- list of legend properties, each element is a dict:
+                'values' -- data values to be written into SLD file.
+                'labels' -- labels corresponding to data values.
+                'rgb' -- hex-values of colors corresponding to data values.
+                'layer_name' -- name of the raster layer.
+                'fill_value' -- values for transparent pixels
         """
-        if fill_value.dtype == np.dtype('bool'):  # Convert boolean fill value to numeric.
-            fill_value = int(fill_value)
+
         sld = {}
         sld['StyledLayerDescriptor'] = {}
         sld['StyledLayerDescriptor']['@version'] = '1.0.0'
@@ -121,34 +177,49 @@ class SLDLegend:
         sld['StyledLayerDescriptor']['@xmlns:ogc'] = 'http://www.opengis.net/ogc'
         sld['StyledLayerDescriptor']['@xmlns:xlink'] = 'http://www.w3.org/1999/xlink'
         sld['StyledLayerDescriptor']['@xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance'
-        sld['StyledLayerDescriptor']['@xsi:schemaLocation'] = 'http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd'
-        sld['StyledLayerDescriptor']['NamedLayer'] = {}
-        sld['StyledLayerDescriptor']['NamedLayer']['Name'] = 'Raster Layer'
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle'] = {}
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['Name'] = 'Raster'
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['Title'] = 'Raster'
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['Abstract'] = 'Colors'
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle'] = {}
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule'] = {}
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['RasterSymbolizer'] = {}
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['RasterSymbolizer']['Opacity'] = 0.8
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['RasterSymbolizer']['ColorMap'] = {}
-        colormap_entry = [{'@color':'#000000', '@quantity':fill_value, '@label':'', '@opacity':'0.0'}]
-        for i, _ in reversed(list(enumerate(legend_values))):
-            colormap_entry.append({'@color':rgb_values[i], '@quantity': legend_values[i], '@label':legend_labels[i]})
-        sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule']['RasterSymbolizer']['ColorMap']['ColorMapEntry'] = colormap_entry
+        sld['StyledLayerDescriptor']['@xsi:schemaLocation'] = \
+            'http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd'
+
+        sld['StyledLayerDescriptor']['NamedLayer'] = []
+        for legend_data in legend_properties:
+            named_layer_entry = {}
+            named_layer_entry['Name'] = legend_data['layer_name']
+            named_layer_entry['UserStyle'] = {}
+            named_layer_entry['UserStyle']['Name'] = 'Raster'
+            named_layer_entry['UserStyle']['Title'] = 'Raster'
+            named_layer_entry['UserStyle']['Abstract'] = 'Colors'
+
+            feature_type_style = {}
+            feature_type_style['Rule'] = {}
+            feature_type_style['Rule']['RasterSymbolizer'] = {}
+            feature_type_style['Rule']['RasterSymbolizer']['Opacity'] = 0.8
+            colormap_entry = [{'@color': '#000000',
+                               '@quantity': legend_data['fill_value'],
+                               '@label': '',
+                               '@opacity': '0.0'}]
+            for i, _ in reversed(list(enumerate(legend_data['values']))):
+                colormap_entry.append({'@color': legend_data['rgb'][i],
+                                       '@quantity': legend_data['values'][i],
+                                       '@label': legend_data['labels'][i]})
+            feature_type_style['Rule']['RasterSymbolizer']['ColorMap'] = {}
+            feature_type_style['Rule']['RasterSymbolizer']['ColorMap']['ColorMapEntry'] = colormap_entry
+            named_layer_entry['UserStyle']['FeatureTypeStyle'] = feature_type_style
+            sld['StyledLayerDescriptor']['NamedLayer'].append(named_layer_entry)
 
         with open(filename, 'w') as file_descriptor:
             file_descriptor.write(xmltodict.unparse(sld, pretty=True))
 
-    def write_stations(self, filename, legend_values, legend_labels, rgb_values):
-        """Writes a legend for stations data into SLD file.
+    def write_stations(self, filename: str, legend_data: dict):
+        """Writes a legend for station data into SLD file.
 
         Arguments:
             filename -- name of SLD file.
-            legend_values -- data values to be written into SLD file.
-            legend_labels -- labels corresponding to data values.
-            rgb_values -- hex-values of colors corresponding to data values.
+            legend_data -- dictionary with legend data:
+                'values' -- data values to be written into SLD file.
+                'labels' -- labels corresponding to data values.
+                'rgb' -- hex-values of colors corresponding to data values.
+                'layer_name' -- name of the named layer.
+                'fill_value' -- values for transparent pixels
         """
 
         field_name = 'VALUE'
@@ -162,7 +233,8 @@ class SLDLegend:
         sld['StyledLayerDescriptor']['@xmlns:ogc'] = 'http://www.opengis.net/ogc'
         sld['StyledLayerDescriptor']['@xmlns:xlink'] = 'http://www.w3.org/1999/xlink'
         sld['StyledLayerDescriptor']['@xmlns:xsi'] = 'http://www.w3.org/2001/XMLSchema-instance'
-        sld['StyledLayerDescriptor']['@xsi:schemaLocation'] = 'http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd'
+        sld['StyledLayerDescriptor']['@xsi:schemaLocation'] = \
+            'http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd'
         sld['StyledLayerDescriptor']['NamedLayer'] = {}
         sld['StyledLayerDescriptor']['NamedLayer']['Name'] = 'Stations'
         sld['StyledLayerDescriptor']['NamedLayer']['UserStyle'] = {}
@@ -171,32 +243,32 @@ class SLDLegend:
         sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle'] = {}
         sld['StyledLayerDescriptor']['NamedLayer']['UserStyle']['FeatureTypeStyle']['Rule'] = []
 
-        n_values = len(legend_values)
+        n_values = len(legend_data['values'])
         for i in range(n_values-1, -1, -1):
             rule = {}
             ogc_filter = {}
             if i == n_values-1: # Top line of the legend
                 rule['Name'] = 'First level'
-                rule['Title'] = '<'+legend_labels[i]
+                rule['Title'] = '<' + legend_data['labels'][i]
                 ogc_filter['ogc:PropertyIsLessThan'] = {}
                 ogc_filter['ogc:PropertyIsLessThan']['ogc:PropertyName'] = field_name
-                ogc_filter['ogc:PropertyIsLessThan']['ogc:Literal'] = legend_values[i]
+                ogc_filter['ogc:PropertyIsLessThan']['ogc:Literal'] = legend_data['values'][i]
             elif i == 0: # Bottom line of the legend
                 rule['Name'] = 'Last level'
-                rule['Title'] = '>'+legend_labels[i]
+                rule['Title'] = '>' + legend_data['labels'][i]
                 ogc_filter['ogc:PropertyIsGreaterThanOrEqualTo'] = {}
                 ogc_filter['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:PropertyName'] = field_name
-                ogc_filter['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:Literal'] = legend_values[i]
+                ogc_filter['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:Literal'] = legend_data['values'][i]
             else:
                 rule['Name'] = 'Next level'
-                rule['Title'] = legend_labels[i] + ' .. ' + legend_labels[i-1]
+                rule['Title'] = legend_data['labels'][i] + ' .. ' + legend_data['labels'][i-1]
                 ogc_filter['ogc:And'] = {}
                 ogc_filter['ogc:And']['ogc:PropertyIsGreaterThanOrEqualTo'] = {}
                 ogc_filter['ogc:And']['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:PropertyName'] = field_name
-                ogc_filter['ogc:And']['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:Literal'] = legend_values[i]
+                ogc_filter['ogc:And']['ogc:PropertyIsGreaterThanOrEqualTo']['ogc:Literal'] = legend_data['values'][i]
                 ogc_filter['ogc:And']['ogc:PropertyIsLessThan'] = {}
                 ogc_filter['ogc:And']['ogc:PropertyIsLessThan']['ogc:PropertyName'] = field_name
-                ogc_filter['ogc:And']['ogc:PropertyIsLessThan']['ogc:Literal'] = legend_values[i-1]
+                ogc_filter['ogc:And']['ogc:PropertyIsLessThan']['ogc:Literal'] = legend_data['values'][i-1]
 
             rule['ogc:Filter'] = ogc_filter
             point_symbolizer = {}
@@ -206,10 +278,10 @@ class SLDLegend:
             point_symbolizer['Graphic']['Mark']['Fill'] = {}
             point_symbolizer['Graphic']['Mark']['Fill']['CssParameter'] = {}
             point_symbolizer['Graphic']['Mark']['Fill']['CssParameter']['@name'] = 'fill'
-            point_symbolizer['Graphic']['Mark']['Fill']['CssParameter']['#text'] = rgb_values[i]
+            point_symbolizer['Graphic']['Mark']['Fill']['CssParameter']['#text'] = legend_data['rgb'][i]
             point_symbolizer['Graphic']['Mark']['Stroke'] = {}
             point_symbolizer['Graphic']['Mark']['Stroke']['CssParameter'] = []
-            point_symbolizer['Graphic']['Mark']['Stroke']['CssParameter'].append({'@name':'stroke', '#text':rgb_values[i]})
+            point_symbolizer['Graphic']['Mark']['Stroke']['CssParameter'].append({'@name':'stroke', '#text':legend_data['rgb'][i]})
             point_symbolizer['Graphic']['Mark']['Stroke']['CssParameter'].append({'@name':'stroke-width', '#text':'1'})
             point_symbolizer['Graphic']['Size'] = symbol_size
             rule['PointSymbolizer'] = point_symbolizer

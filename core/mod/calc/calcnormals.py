bff3844be28cb984b2d90e0c772c379adbee2293
Anna <r.ann.1194@gmail.com>
2020-03-13 15:09:44 +0700

    add month to normals

--- core/mod/calc/calcnormals.py
+++ core/mod/calc/calcnormals.py
@@ -36,25 +36,24 @@ class CalcNormals(Calc):
         end_date = datetime.datetime.strptime(time_segment['@ending'], '%Y%m%d%H')
         years = [start_date.year + i for i in range(end_date.year - start_date.year + 1)]
         
+        normals = {}
+        all_segments_data = []
+
         if calc_mode == 'day':
             segment_start = datetime.datetime(1, start_date.month, start_date.day, start_date.hour)
             segment_end = datetime.datetime(1, end_date.month, end_date.day, end_date.hour)
             dates_delta = segment_end - segment_start + datetime.timedelta(days=1)  # Days in the segment.
-            days = [segment_start + datetime.timedelta(days=i) for i in range(dates_delta.days)]  # Days of the segment.
-        elif calc_mode == 'month':
-            months = [start_date.month + i for i in range(end_date.month - start_date.month + 1)] # Months of the segment.
-
-        # For each day of the year (segment) this day for all years (30).
-        # Concatenate for 30 years to obtain 30 lon-lat grids.
-        # Calculate normals along time axis for each cell of the grid to get 2-D grid.
-        # Make a Masked array using mask from one of the 1-day arrays.
-        # We suppose that the mask is the same for all lon-lat grids along the time axis.
-        # Concatenate 2-D grids for all days along time axis to get [time, lat, lon] result array.
-        normals = {}
-        all_days_data = []
+            days_grid = [segment_start + datetime.timedelta(days=i) for i in range(dates_delta.days)]  # Days of the segment.
+            segments_grid = days_grid
             
-        if calc_mode == 'day':
-            for day in days:
+            # For each day of the year (segment) this day for all years (30).
+            # Concatenate for 30 years to obtain 30 lon-lat grids.
+            # Calculate normals along time axis for each cell of the grid to get 2-D grid.
+            # Make a Masked array using mask from one of the 1-day arrays.
+            # We suppose that the mask is the same for all lon-lat grids along the time axis.
+            # Concatenate 2-D grids for all days along time axis to get [time, lat, lon] result array.
+
+            for day in days_grid:
                 segments = []
                 for year in years:
                     day_i = datetime.datetime(year, day.month, day.day, day.hour, day.minute)
@@ -66,13 +65,18 @@ class CalcNormals(Calc):
                 result = self._data_helper.get(uid, segments=segments, levels=level)
                 data = np.ma.stack(
                     [result['data'][level]['Year {}'.format(year)]['@values'] for year in years])
-                all_days_data.append(np.ma.mean(data, axis=0))
+                all_segments_data.append(np.ma.mean(data, axis=0))
+        
         elif calc_mode == 'month':
-            for month in months:
+            months = [start_date.month + i for i in range(end_date.month - start_date.month + 1)] # Months of the segment.
+            months_grid = [datetime.datetime(1, mi, 1, 0) for mi in months]
+            segments_grid = days_grid
+
+            for month in months_grid:
                 segments = []
                 for year in years:
                     month_i = datetime.datetime(year, month.month, month.day, month.hour, month.minute)
-                    one_month = {}  # 1-day segment to read.
+                    one_month = {}  # 1-month segment to read.
                     one_month['@beginning'] = month_i.strftime('%Y%m%d%H')
                     one_month['@ending'] = month_i.strftime('%Y%m%d23')
                     one_month['@name'] = 'Year {}'.format(year)
@@ -80,18 +84,20 @@ class CalcNormals(Calc):
                 result = self._data_helper.get(uid, segments=segments, levels=level)
                 data = np.ma.stack(
                     [result['data'][level]['Year {}'.format(year)]['@values'] for year in years])
-                all_days_data.append(np.ma.mean(data, axis=0))
+                all_segments_data.append(np.ma.mean(data, axis=0))
+        else:
+            self.logger.error('Error! Unknown calculation mode value: \'%s\'', calc_mode)
+            raise ValueError 
 
-
-        normals_data = np.stack(all_days_data)  # Stack to array.
+        normals_data = np.stack(all_segments_data)  # Stack to array.
         mask_0 = result['data'][level]['Year {}'.format(years[0])]['@values'].mask  # lon-lat mask.
         mask_shape = [1] * (mask_0.ndim + 1)  # New shape of the mask.
-        mask_shape[0] = len(days)  # Set it to be (n_days, 1, 1) or (n_days, 1).
+        mask_shape[0] = len(segments_grid)  # Set it to be (n_days, 1, 1) or (n_days, 1).
         mask = np.tile(mask_0, mask_shape)  # Generate a mask to conform data dimensions.
         normals['data'] = np.ma.MaskedArray(normals_data, mask=mask, fill_value=result['@fill_value'])
 
         normals['@base_period'] = time_segment
-        normals['@day_grid'] = days
+        normals['@day_grid'] = segments_grid
         normals['@longitude_grid'] = result['@longitude_grid']
         normals['@latitude_grid'] = result['@latitude_grid']
         normals['@fill_value'] = result['@fill_value']

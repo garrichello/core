4db35bde7018b076f543ed1f1563691d3de201a9
Igor Okladnikov <oig@ngs.ru>
2020-03-29 11:09:43 +0700

    Add type data to result

--- core/mod/data/data.py
+++ core/mod/data/data.py
@@ -17,7 +17,7 @@ class Data:
     def __init__(self, data_info):
         self.logger = logging.getLogger()
         self._data_info = data_info
-        self._read_result = {}   # Data arrays, grids and some additional information read in a child class.
+        self._read_result = {'@type': 'data'}   # Data arrays, grids and some additional information read in a child class.
         self._read_result['data'] = {}  # Contains data arrays read at each vertical level.
         self._data_by_segment = {}  # Data for each time segment for each vertical level.
 

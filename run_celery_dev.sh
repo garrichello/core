978f32e4fb05be6d71f4027ece8f49531850f228
Igor Okladnikov <oig@ngs.ru>
2020-05-19 16:28:16 +0600

    Increase workers pool for dev server

--- run_celery_dev.sh
+++ run_celery_dev.sh
@@ -1,2 +1,2 @@
-virtualenv/bin/celery worker --logfile=logs/%n%I.log --loglevel=INFO -E -n debugworker@%h -c 2 --config celeryconfig_dev
+virtualenv/bin/celery worker --logfile=logs/%n%I.log --loglevel=INFO -E -n debugworker@%h -c 4 --config celeryconfig_dev
 
